e1ed987ba50ae7b56893540f78d45d2bf48f15ce
michael Chimezie <mikeyeucresta@gmail.com>
2024-06-24 19:20:34 +0100

    Update core.dart

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -119,8 +119,7 @@ class Core with Events implements ICore {
       logger.i('Core Initilization Success');
     } catch (error) {
       logger.w(
-          'Core Initilization Failure at epoch ${DateTime.now().millisecondsSinceEpoch}',
-          error);
+          'Core Initilization Failure at epoch ${DateTime.now().millisecondsSinceEpoch}: $error');
       rethrow;
     }
   }
